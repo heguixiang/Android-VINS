955a99240c09df21c92c1239eb07f8021abbb54e
solomonhe <solomonhe@zhaoxin.com>
2017-09-29 10:24:39 +0800

    add app/src/main/cpp/src code for VINS-Mono

---
+++ app/src/main/cpp/src/vins_estimator/src/parameters.cpp
@@ -0,0 +1,183 @@
+#include "parameters.h"
+#include <string>
+#include <unistd.h>
+#include <iostream>
+
+#include <android/log.h>
+#define LOG_TAG "vins_parameter"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+#define FILENAMEPATH_MAX 80
+using namespace std;
+
+double INIT_DEPTH;
+double MIN_PARALLAX;
+double ACC_N, ACC_W;
+double GYR_N, GYR_W;
+
+std::vector<Eigen::Matrix3d> RIC;
+std::vector<Eigen::Vector3d> TIC;
+
+Eigen::Vector3d G{0.0, 0.0, 9.8};
+
+double BIAS_ACC_THRESHOLD;
+double BIAS_GYR_THRESHOLD;
+double SOLVER_TIME;
+int NUM_ITERATIONS;
+int ESTIMATE_EXTRINSIC;
+std::string EX_CALIB_RESULT_PATH;
+std::string VINS_RESULT_PATH;
+int LOOP_CLOSURE = 0;
+int MIN_LOOP_NUM;
+std::string CAM_NAMES_ESTIMATOR;   //add
+std::string PATTERN_FILE;
+std::string VOC_FILE;
+std::string IMAGE_TOPIC;
+std::string IMU_TOPIC;
+int IMAGE_ROW, IMAGE_COL;
+std::string VINS_FOLDER_PATH;
+int MAX_KEYFRAME_NUM;
+
+//feature tracker section
+std::vector<std::string> CAM_NAMES;
+std::string FISHEYE_MASK;
+int MAX_CNT;
+int MIN_DIST;
+int WINDOW_SIZE_FEATURE_TRACKER;
+int FREQ;
+double F_THRESHOLD;
+int SHOW_TRACK;
+int STEREO_TRACK;
+int EQUALIZE;
+int ROW;
+int COL;
+int FOCAL_LENGTH;
+int FISHEYE;
+bool PUB_THIS_FRAME;
+
+//void readParameters(const string & config_file)
+void readParameters(const string & config_file)
+{
+    //string config_file1 = "/data/local/tmp/com.hezhaoxin.solomon.vins_android/app/src/main/cpp/src/config/euroc/euroc_config.yaml";
+    cv::FileStorage fsSettings(config_file.c_str(), cv::FileStorage::READ);
+ //   cv::FileStorage fsSettings(config_file.c_str(), cv::FileStorage::READ);
+    if(!fsSettings.isOpened())
+    {
+        std::cerr << "ERROR: Wrong path to settings " << config_file << std::endl;
+    }
+
+
+ //   VINS_FOLDER_PATH = getcwd(NULL,FILENAMEPATH_MAX);
+ //   fsSettings["output_path"] >> VINS_RESULT_PATH;
+  // VINS_FOLDER_PATH = "/storage/emulated/0/storage/emulated/0/vins/";
+ VINS_FOLDER_PATH = "/data/vins/";
+
+    IMAGE_COL = fsSettings["image_width"];
+    IMAGE_ROW = fsSettings["image_height"];
+
+    SOLVER_TIME = fsSettings["max_solver_time"];
+    NUM_ITERATIONS = fsSettings["max_num_iterations"];
+    MIN_PARALLAX = fsSettings["keyframe_parallax"];
+
+    fsSettings["output_path"] >> VINS_RESULT_PATH;
+    VINS_RESULT_PATH = VINS_FOLDER_PATH + VINS_RESULT_PATH;
+    cout << VINS_RESULT_PATH << endl;
+    std::ofstream foutC(VINS_RESULT_PATH, std::ios::out);
+    foutC.close();
+
+    ACC_N = fsSettings["acc_n"];
+    ACC_W = fsSettings["acc_w"];
+    GYR_N = fsSettings["gyr_n"];
+    GYR_W = fsSettings["gyr_w"];
+    G.z() = fsSettings["g_norm"];
+    LOGD("ACC_N:%f , ACC_W:%f ,GYR_N:%f , GYR_W:%f , G.z():%f",ACC_N, ACC_W, GYR_N, GYR_W,  G.z() );
+    ESTIMATE_EXTRINSIC = fsSettings["estimate_extrinsic"];
+    if (ESTIMATE_EXTRINSIC == 2)
+    {
+      //  ROS_WARN("have no prior about extrinsic param, calibrate extrinsic param");
+	cout << "WARN: have no prior about extrinsic param, calibrate extrinsic param" << endl;
+        RIC.push_back(Eigen::Matrix3d::Identity());
+        TIC.push_back(Eigen::Vector3d::Zero());
+        fsSettings["ex_calib_result_path"] >> EX_CALIB_RESULT_PATH;
+        EX_CALIB_RESULT_PATH = VINS_FOLDER_PATH + EX_CALIB_RESULT_PATH;
+
+    }
+    else
+    {
+        if ( ESTIMATE_EXTRINSIC == 1)
+        {
+          //  ROS_WARN(" Optimize extrinsic param around initial guess!");
+	  cout << "WARN: Optimize extrinsic param around initial guess!" << endl;
+            fsSettings["ex_calib_result_path"] >> EX_CALIB_RESULT_PATH;
+            EX_CALIB_RESULT_PATH = VINS_FOLDER_PATH + EX_CALIB_RESULT_PATH;
+        }
+        if (ESTIMATE_EXTRINSIC == 0)
+          //  ROS_WARN(" fix extrinsic param ");
+	  cout << "WARN: fix extrinsic param "<< endl;
+
+        cv::Mat cv_R, cv_T;
+        fsSettings["extrinsicRotation"] >> cv_R;
+        fsSettings["extrinsicTranslation"] >> cv_T;
+        Eigen::Matrix3d eigen_R;
+        Eigen::Vector3d eigen_T;
+        cv::cv2eigen(cv_R, eigen_R);
+        cv::cv2eigen(cv_T, eigen_T);
+        Eigen::Quaterniond Q(eigen_R);
+        eigen_R = Q.normalized();
+        RIC.push_back(eigen_R);
+        TIC.push_back(eigen_T);
+
+	LOGD("Extrinsic_R : ");
+	LOGD("Extrinsic_R : %f            %f               %f",  RIC[0](0,0), RIC[0](0,1), RIC[0](0,2));
+	LOGD("Extrinsic_R : %f            %f               %f",  RIC[0](1,0), RIC[0](1,1), RIC[0](1,2));
+	LOGD("Extrinsic_R : %f            %f               %f",  RIC[0](2,0), RIC[0](2,1), RIC[0](2,2));
+	LOGD("Extrinsic_T : %f   %f   %f", TIC[0].transpose()(0), TIC[0].transpose()(1), TIC[0].transpose()(2) );
+
+    }
+
+
+
+    LOOP_CLOSURE = fsSettings["loop_closure"];
+    //LOOP_CLOSURE = 0;
+    if (LOOP_CLOSURE == 1)
+    {
+        fsSettings["voc_file"] >> VOC_FILE;;
+        fsSettings["pattern_file"] >> PATTERN_FILE;
+        MIN_LOOP_NUM = fsSettings["min_loop_num"];
+        CAM_NAMES_ESTIMATOR = config_file;   //add
+    }
+
+
+    INIT_DEPTH = 5.0;
+    BIAS_ACC_THRESHOLD = 0.1;
+    BIAS_GYR_THRESHOLD = 0.1;
+    MAX_KEYFRAME_NUM = 1000;
+
+    // feature tracker
+    MAX_CNT = fsSettings["max_cnt"];
+    MIN_DIST = fsSettings["min_dist"];
+    ROW = fsSettings["image_height"];
+    COL = fsSettings["image_width"];
+   FREQ = fsSettings["freq"];
+    F_THRESHOLD = fsSettings["F_threshold"];
+    SHOW_TRACK = fsSettings["show_track"];
+   EQUALIZE = fsSettings["equalize"];
+    FISHEYE = fsSettings["fisheye"];
+    if (FISHEYE == 1)
+        FISHEYE_MASK = VINS_FOLDER_PATH + "/src/config/fisheye_mask.jpg";
+    CAM_NAMES.push_back(config_file);
+	//ROS_INFO_STREAM("CAM_NAMES" << CAM_NAMES.front());
+    WINDOW_SIZE_FEATURE_TRACKER = 20;
+    STEREO_TRACK = false;
+    FOCAL_LENGTH = 460;
+    PUB_THIS_FRAME = false;
+
+    MIN_PARALLAX = MIN_PARALLAX / FOCAL_LENGTH;
+    if (FREQ == 0)
+        FREQ = 100;
+
+   LOGD("VOC_FILE: %s", VOC_FILE.c_str());
+   LOGD("PATTERN_FILE: %s", PATTERN_FILE.c_str());
+    fsSettings.release();
+}
